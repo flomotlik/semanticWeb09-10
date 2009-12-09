7f0140ecff2436e642cc6efc7322e88aed260809
NonSense86 <nonsense86@gmail.com>
2009-12-09 12:39:30 +0100

    -Umlaute umgeschrieben -Menu verbessert, liest nicht nur integer -SPARQL abfragen ausgebessert, keine URIs werden mehr ausgegeben

--- abgabe1/src/main/java/at/tuwien/semanticWeb/abgabe1/Main.java
+++ abgabe1/src/main/java/at/tuwien/semanticWeb/abgabe1/Main.java
@@ -8,40 +8,33 @@ public class Main {
         Main main = new Main();
         boolean exit = false;
         Sparqler sparqler = new Sparqler();
+        String input;
         do {
-            switch (main.printMenu()) {
-                case 1:
-                    sparqler.first();
-                    break;
-                case 2:
-                    sparqler.second();
-                    break;
-                case 3:
-                    sparqler.third();
-                    break;
-                case 4:
-                    sparqler.fourth();
-                    break;
-                case 5:
-                    sparqler.fifth();
-                    break;
-                case 6:
-                    sparqler.sixth();
-                    break;
-                case 7:
-                    sparqler.seventh();
-                    break;
-                case 8:
-                    sparqler.eigth();
-                    break;
-                case 9:
-                    exit = true;
-                    break;
-            }
+            input = main.printMenu();
+            if (input.equals("1")) 
+            	sparqler.first();
+            else if (input.equals("2"))
+            	sparqler.second();
+            else if (input.equals("3"))
+            	sparqler.third();
+            else if (input.equals("4"))
+            	sparqler.fourth();
+            else if (input.equals("5"))
+            	sparqler.fifth();
+            else if (input.equals("6"))
+            	sparqler.sixth();
+            else if (input.equals("7"))
+            	sparqler.seventh();
+            else if (input.equals("8"))
+            	sparqler.eigth();
+            else if (input.equalsIgnoreCase("q")) {
+            	break;
+            } else
+            	System.out.println("Falsche eingabe.");
         } while (!exit);
     }
 
-    public int printMenu() {
+    public String printMenu() {
         System.out.println("HotelExpert\n" + "-----------\n" + "1 Welche Hotels haben mindestens <Anzahl> Sterne?\n"
             + "2 Welche Attraktionen oder Hotels sind in der Naehe von <Hotelname>?\n"
             + "3 Hat Person <Gastname> schon in Hotel <Hotelname> gewohnt (ja/nein)?\n"
@@ -49,8 +42,9 @@ public class Main {
             + "5 Welche Wellnessangebote werden von Gaesten genutzt, die ein Raucherzimmer gebucht haben?\n"
             + "6 Ist das Zimmer <Zimmernummer1> neben dem Zimmer <Zimmernummer2> (ja/nein)?\n"
             + "7 Ist <Gastname> verwandt mit <Gastname> (ja/nein)?\n"
-            + "8 Welche Gaeste, die verwandt sind, haben neben einander ein Zimmer gebucht?\n" + "9 Beenden\n"
+            + "8 Welche Gaeste, die verwandt sind, haben neben einander ein Zimmer gebucht?\n" 
+            + "Q Beenden\n"
             + "Ihre Auswahl:");
-        return new Scanner(System.in).nextInt();
+        return new Scanner(System.in).next();
     }
 }
--- abgabe1/src/main/java/at/tuwien/semanticWeb/abgabe1/Sparqler.java
+++ abgabe1/src/main/java/at/tuwien/semanticWeb/abgabe1/Sparqler.java
@@ -1,72 +1,108 @@
 package at.tuwien.semanticWeb.abgabe1;
 
-import java.net.URL;
 import java.util.Scanner;
 
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.util.FileManager;
 
 public class Sparqler {
+	
+	private Model model;
+	
+	public Sparqler() {
+		model = FileManager.get().loadModel("tourismus.owl");
+	}
 
     public void first() {
         String number = this.askParameter("Anzahl");
-        String query = "SELECT ?Hotel WHERE {  ?Hotel trsm:hatSterne ?sterne FILTER(?sterne >= " + number + ")}";
-        printQuery(query);
+        String query = "SELECT ?x " +
+        				"WHERE {?hotel trsm:hatSterne ?sterne ; " +
+        						"trsm:hatName ?x " +
+        						"FILTER(?sterne >= " + number + ")}";
+        printSelectQuery(query);
     }
 
     public void second() {
         String hotel = this.askParameter("Hotel");
-        String query = "SELECT ?inNaehe WHERE {  ?hotel trsm:hatName \"" + hotel + "\" . ?hotel trsm:istInNaeheVon ?inNaehe}";
-        printQuery(query);
+        String query = "SELECT ?x " +
+        				"WHERE {?hotel trsm:hatName \"" + hotel + "\" ; " +
+        						"trsm:istInNaeheVon ?inNaehe ." +
+        						"{?inNaehe trsm:hatBezeichnung ?x} " +
+        						"UNION " + 
+        						"{?inNaehe trsm:hatName ?x}}";
+        printSelectQuery(query);
     }
 
     public void third() {
         String name = this.askParameter("Gastname");
         String hotel = this.askParameter("Hotelname");
-        String query = "SELECT ?hotel WHERE { ?guest trsm:hatName "
-            + "\"" + name + "\"" 
-            + ". ?guest trsm:bucht ?buchung. ?buchung trsm:beinhaltetZimmer ?zimmer. ?zimmer trsm:gehoertZuHotel ?hotel.?hotel trsm:hatName "
-            + "\"" + hotel + "\"}";
-        printYesNoQuery(query);
+        String query = "ASK {?guest trsm:hatName " + "\"" + name + "\"" +
+            ". ?guest trsm:bucht ?buchung . " +
+            "?buchung trsm:beinhaltetZimmer ?zimmer . " +
+            "?zimmer trsm:gehoertZuHotel ?hotel . " +
+            "?hotel trsm:hatName " + "\"" + hotel + "\"}";
+        printAskQuery(query);
     }
 
     public void fourth() {
-        String query = "SELECT ?guest WHERE { ?guest trsm:bucht ?buchung. ?buchung trsm:beinhaltetZimmer ?zimmer. ?zimmer trsm:istRaucherZimmer true }";
-        printQuery(query);
+        String query = "SELECT ?x " +
+        				"WHERE {?guest trsm:bucht ?buchung . " +
+        				"?buchung trsm:beinhaltetZimmer ?zimmer . " +
+        				"?zimmer trsm:istRaucherZimmer true . " +
+        				"?guest trsm:hatName ?x}";
+        printSelectQuery(query);
     }
 
     public void fifth() {
-        String query = "SELECT ?angebot WHERE { ?guest trsm:bucht ?buchung. ?buchung trsm:beinhaltetZimmer ?zimmer. ?zimmer trsm:istRaucherZimmer true.?guest trsm:nutzt ?angebot}";
-        printQuery(query);
+        String query = "SELECT ?x " +
+        				"WHERE { ?guest trsm:bucht ?buchung . " +
+        				"?buchung trsm:beinhaltetZimmer ?zimmer . " +
+        				"?zimmer trsm:istRaucherZimmer true . " +
+        				"?guest trsm:nutzt ?angebot . " +
+        				"?angebot trsm:hatBezeichnung ?x}";
+        printSelectQuery(query);
     }
 
     public void sixth() {
         String number1 = this.askParameter("Zimmernummer 1");
         String number2 = this.askParameter("Zimmernummer 2");
-        String query = "SELECT ?zimmer2 WHERE { ?zimmer trsm:liegtNeben ?zimmer2. ?zimmer trsm:hatNummer ?nr1.?zimmer2 trsm:hatNummer ?nr2 FILTER(?nr1 = "
+        String query = "ASK {?zimmer trsm:liegtNeben ?zimmer2 . " +
+        				"?zimmer trsm:hatNummer ?nr1 . " +
+        				"?zimmer2 trsm:hatNummer ?nr2 FILTER(?nr1 = "
             + number1 + " && ?nr2 = " + number2 + ")}";
-        printYesNoQuery(query);
+        printAskQuery(query);
     }
 
     public void seventh() {
         String guest1 = this.askParameter("Gast 1");
         String guest2 = this.askParameter("Guest 2");
-        String query = "SELECT ?guest1 ?guest2 WHERE {?guest1 trsm:hatName \"" + guest1 + "\" . ?guest2 trsm:hatName "
-            + "\"" + guest2 + "\""
-            + ".{{?guest1 trsm:istAllgemeinVerwandt ?guest2} UNION {?guest1 trsm:istFamilienVerwandt ?guest2}}}";
-        printYesNoQuery(query);
+        String query = "ASK {?guest1 trsm:hatName \"" + guest1 + "\" . " +
+        				"?guest2 trsm:hatName " + "\"" + guest2 + "\"" +
+        				".{{?guest1 trsm:istAllgemeinVerwandt ?guest2} " +
+        				"UNION " +
+        				"{?guest1 trsm:istFamilienVerwandt ?guest2}}}";
+        printAskQuery(query);
     }
 
     public void eigth() {
-        String query = "SELECT ?guest1 ?guest2 WHERE { "
-            + "{{?guest1 trsm:istAllgemeinVerwandt ?guest2} UNION {?guest1 trsm:istFamilienVerwandt ?guest2}}. "
-            + "?guest1 trsm:bucht ?buchung1. ?buchung1 trsm:beinhaltetZimmer ?zimmer1. "
-            + "?guest2 trsm:bucht ?buchung2. ?buchung2 trsm:beinhaltetZimmer ?zimmer2. "
-            + "?zimmer1 trsm:liegtNeben ?zimmer2}";
-        printQuery(query);
+        String query = "SELECT ?x " +
+        				"WHERE { " +
+        				"{?guest1 trsm:istAllgemeinVerwandt ?guest2} " +
+        				"UNION " +
+        				"{?guest1 trsm:istFamilienVerwandt ?guest2} . " +
+        				"?guest1 trsm:bucht ?buchung1 . " +
+        				"?buchung1 trsm:beinhaltetZimmer ?zimmer1 . " +
+        				"?guest2 trsm:bucht ?buchung2 . " +
+        				"?buchung2 trsm:beinhaltetZimmer ?zimmer2 . " +
+        				"?zimmer1 trsm:liegtNeben ?zimmer2 . " +
+        				"?guest1 trsm:hatName ?x}";
+        printSelectQuery(query);
     }
 
     private String askParameter(String paramName) {
@@ -74,41 +110,59 @@ public class Sparqler {
         return new Scanner(System.in).nextLine();
     }
     
-    private void printQuery(String queryString){
+    private void printSelectQuery(String queryString){
         ResultSet results = this.query(queryString);
         if (results != null) {
             System.out.println("Result:");
             while (results.hasNext()) {
-                System.out.println("   " + results.next());
+            	QuerySolution qs = results.next();
+            	RDFNode rdfNode = qs.get("x");
+            	if (rdfNode.isLiteral())
+            		System.out.println("\t" + rdfNode.as(Literal.class).getString());
+            	else
+            		System.out.println("\t" + rdfNode.toString());
+                
             }
-            System.out.print("Dr�cken sie <Eingabe> um zum Men� zur�ckzukehren");
+            System.out.print("Druecken sie <Eingabe> um zum Menue zurueckzukehren");
             new Scanner(System.in).nextLine();
         }
     }
     
-    private void printYesNoQuery(String queryString){
-        ResultSet results = this.query(queryString);
-        if(results.hasNext()){
-            System.out.println("True");
-        }else{
-            System.out.println("False");
-        }
-        System.out.print("Dr�cken sie <Eingabe> um zum Men� zur�ckzukehren");
+    private void printAskQuery(String queryString){
+        try {
+        	if (this.askQuery(queryString))
+        		System.out.println("Ja");
+        	else
+        		System.out.println("Nein");
+		} catch (Exception e) {
+			System.out.println("Entschuldigung, beim Verarbeiten der Abfrage ist ein Fehler aufgetreten: " + e.getMessage());
+		}
+        System.out.print("Druecken sie <Eingabe> um zum Menue zurueckzukehren");
         new Scanner(System.in).nextLine();
     }
-
+	
     private ResultSet query(String queryString) {
-    	// TODO: m�ssen wir wirklich jedes Mal das Model neu laden, oder d�rfen wir das wiederverwenden? - wahrscheinlich schon
     	try {
-			Model model = FileManager.get().loadModel("tourismus.owl");
 			String newQueryString = "PREFIX trsm:<http://www.owl-ontologies.com/tourism.owl#> PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>" + queryString;
 			QueryExecution qe = QueryExecutionFactory.create(newQueryString, model);
 			qe.close();
-                        System.out.flush();
-                        return qe.execSelect();
+            System.out.flush();
+            return qe.execSelect();
 		} catch (Throwable e) {
 			System.out.println("Entschuldigung, beim Verarbeiten der Abfrage ist ein Fehler aufgetreten: " + e.getMessage());
 			return null;
+		}
+    }
+    
+    private boolean askQuery(String queryString) throws Exception {
+    	try {
+			String newQueryString = "PREFIX trsm:<http://www.owl-ontologies.com/tourism.owl#> PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>" + queryString;
+			QueryExecution qe = QueryExecutionFactory.create(newQueryString, model);
+			qe.close();
+            System.out.flush();
+            return qe.execAsk();
+		} catch (Throwable e) {
+			throw new Exception(e.getMessage());
 		}
     }
 }
